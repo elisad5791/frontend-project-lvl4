b89ec5b03a94b1b89f67222fc4bf9e71d0154e4b
elisad5791 <elisad5791@mail.ru>
2022-05-21 00:56:57 +0500

    fix

--- src/components/privateRoute.jsx
+++ src/components/privateRoute.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import routes from '../routes.js';
 import { useSelector } from 'react-redux';
+import routes from '../routes.js';
 
 function PrivateRoute({ children }) {
   const isAuthenticated = useSelector((state) => state.app.isAuthenticated);
   return (
     <Route
-      render={() => (isAuthenticated ? (children) : <Redirect to={{ pathname: routes.loginPagePath() }} />)}
+      render={() => (isAuthenticated
+        ? (children)
+        : <Redirect to={{ pathname: routes.loginPagePath() }} />)}
     />
   );
 }
