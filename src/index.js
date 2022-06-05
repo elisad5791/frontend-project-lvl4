69a58873cc3f70089a3c90f4b6cd0fbd24a5b111
elisad5791 <elisad5791@mail.ru>
2022-06-05 14:40:39 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import ReactDOM from 'react-dom';
 import '../assets/application.scss';
 import init from './init.jsx';
 
+window.onload = function hideLoader() {
+  document.body.classList.add('loaded_hiding');
+  window.setTimeout(() => {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 500);
+};
+
 const runApp = async () => {
   const socket = io();
   const vdom = await init(socket);
